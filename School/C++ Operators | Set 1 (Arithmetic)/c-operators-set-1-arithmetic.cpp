76a50a6d7297e398d26ee0b4a8568e412f1e6ca8
Khushi Singh <khushiisingh100@gmail.com>
2023-09-30 20:48:55 +0530

    Added Solution - GfG to GitHub

---
+++ School/C++ Operators | Set 1 (Arithmetic)/c-operators-set-1-arithmetic.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> cppOperators(int A, int B) {
+        // code here
+        if(A>B){
+            return{A+B, A*B, A-B, A/B}; 
+        }
+        else{
+            return{A+B, A*B, B-A, B/A};
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int A, B;
+        cin >> A >> B;
+        Solution ob;
+        vector<int> ans = ob.cppOperators(A, B);
+        for (int u : ans) cout << u << "\n";
+    }
+}
+// } Driver Code Ends
