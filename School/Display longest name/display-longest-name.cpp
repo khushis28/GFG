2c8c78811cbe9703bf15bcc84a561fb1851c1a60
Khushi Singh <khushiisingh100@gmail.com>
2023-10-09 21:56:43 +0530

    Added Solution - GfG to GitHub

---
+++ School/Display longest name/display-longest-name.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    string longest(string names[], int n)
+    {
+        string longest_name=names[0];
+        for(int i=1; i<n; i++){
+            if(names[i].length()>longest_name.length()){
+                longest_name=names[i];
+            }
+        }
+        return longest_name;
+    } 
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		string names[n];
+		
+		for(int i=0;i<n;i++)
+			cin>>names[i];
+		Solution ob;
+		cout<< ob.longest(names, n) << endl;
+	}
+}
+
+// } Driver Code Ends
