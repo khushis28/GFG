805c1bf73ca6095eab1f509fd4560c4e8762c2e2
Khushi Singh <khushiisingh100@gmail.com>
2023-10-15 19:33:24 +0530

    Added Solution - GfG to GitHub

---
+++ School/Smaller and Larger/smaller-and-larger.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+     vector<int> getMoreAndLess(int arr[], int n, int x){
+         vector<int> result;
+         int less=0;
+         int more=0;
+         for(int i=0; i<n; i++){
+             if(arr[i]<=x){
+                 less++;
+             }
+             if(arr[i]>=x){
+                 more++;
+             }
+         }
+         result.push_back(less);
+         result.push_back(more);
+         return result;
+     }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.getMoreAndLess(arr, n, x);
+        cout << ans[0] << " " << ans[1] << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
