6bf7e0b4068c95b7231b294ac54de29ac910be9f
Khushi Singh <khushiisingh100@gmail.com>
2023-11-30 14:01:43 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Shortest path from 1 to n/shortest-path-from-1-to-n.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+//Initial template for c++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{   
+public:
+    int minimumStep(int n){
+        //complete the function here
+        int ans = 0;
+        while(n != 1) {
+            n = (n%3) ? n-1 : n/3;
+            ans++;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.minimumStep(n) << endl;
+    
+    }
+    return 0; 
+} 
+
+
+// } Driver Code Ends
